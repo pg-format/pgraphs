615343c4b1498f3f928f56d7e4c2f68e159f6f16
Jakob Voss <voss@gbv.de>
2024-04-09 13:47:09 +0200

    fix typo

---
+++ src/format/mermaid/index.js
@@ -0,0 +1,21 @@
+// import serialize from "./serializer.js"
+
+export default {
+  name: "Meermaid Flowchart",
+  url: "https://mermaid.js.org/syntax/flowchart.html",
+
+  direction: "mixed",
+  edgeAttributes: false,
+  edgeName: true,
+  edgeTypes: false,
+  graphAttributes: false,
+  hierarchy: true,
+  hyperEdges: false,
+  multiEdges: true,
+  nodeAttributes: false,
+  nodeName: true,
+  nodeTypes: false,
+  visualAttributes: true,
+
+  // serialize,
+}
