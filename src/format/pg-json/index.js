74d6820d44afb2c9ba875211286ee2e56c9d9a62
Jakob Voss <voss@gbv.de>
2024-04-17 12:54:29 +0200

    Partly validate PG-JSON

--- src/format/pg-json/index.js
+++ src/format/pg-json/index.js
@@ -1,9 +1,34 @@
+import { ParsingError } from "../../error.js"
+
 function parse(string) {
-  const graph = JSON.parse(string)
-  // TODO: validate!
-  // - Disallow repeated edge identifiers
-  // 
-  return graph
+  var { nodes, edges } = JSON.parse(string)
+  nodes ??= []
+  edges ??= []
+
+  const nodeIds = new Set()
+  for (let node of nodes) {
+    if (nodeIds.has(node.id)) {
+      throw new ParsingError(`Repeated node identifier "${node.id}"`)
+    }
+    nodeIds.add(node.id)
+  }
+  
+  const edgeIds = new Set()
+  for (let edge of edges) {
+    if (edgeIds.has(edge.id)) {
+      throw new ParsingError(`Repeated edge identifier "${edge.id}"`)
+    }
+    // add implicit nodes
+    if (!nodeIds.has(edge.from)) {
+      nodes.push({ id: edge.from, labels: [], properties: {} })
+    }
+    if (!nodeIds.has(edge.to)) {
+      nodes.push({ id: edge.to, labels: [], properties: {} })
+    }
+    edgeIds.add(edge.id)
+  }
+
+  return { nodes, edges }
 }
 
 export default {
