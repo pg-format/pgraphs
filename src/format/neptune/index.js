b2da1559f5e17904eb41064825e522cb37e1a1cc
Jakob Voss <voss@gbv.de>
2024-05-03 07:58:58 +0200

    Add missing file

---
+++ src/format/neptune/index.js
@@ -0,0 +1,8 @@
+import serialize from "./serialize.js"
+
+export default {
+  name: "Neptune CSV import (aka Gremlin load data format)",
+  url: "https://docs.aws.amazon.com/neptune/latest/userguide/load-data.html",
+
+  serialize,
+}
