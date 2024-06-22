618e6b44c5b30cdfbc4852ddac3563f9a76c930c
Jakob Voss <voss@gbv.de>
2024-06-22 22:47:16 +0200

    Update YARS-PG features

--- src/format/yarspg/index.js
+++ src/format/yarspg/index.js
@@ -2,5 +2,30 @@ import serialize from "./serializer.js"
 
 export default {
   name: "YARS-PG",
+  direction: "mixed",
+
+  nodeTypes: false,
+  edgeTypes: false,
+  nodeName: false,
+  edgeName: false,
+  edgeIdentifier: true, // optional
+  edgeWeight: false,
+
+  graphAttributes: true, // TODO: graph labels are possible too!
+
+  nodeAttributes: true,
+  edgeAttributes: true,
+  visualAttributes: false,
+
+  hierarchy: false,
+  hyperEdges: false,
+
+  // TODO: nodes and edges can be assignde to mutliple graphs
+  // TODO: metadata and meta-properties
+
+  // types are supported via schema only
+  // datatypes: ...
+
+  url: "https://github.com/lszeremeta/yarspg?tab=readme-ov-file#yars-pg-grammar",
   serialize
 }
